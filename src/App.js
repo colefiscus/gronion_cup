66dfdc557d75389fe61e29a4f40ea413736f4242
colefiscus <coletfiscus@gmail.com>
2021-10-02 12:15:34 -0600

    Add initial files and strucures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
+    <>
+    <Header />
     <div className="App">
       
     </div>
+    </>
   );
 }
 
---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+// import GameList from 
+
+const Home = () => {
+
+
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default Home;
