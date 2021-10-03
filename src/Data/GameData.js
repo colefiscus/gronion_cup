a00aa5aa309120fcb3b3391d01b9d33f77799b9a
colefiscus <coletfiscus@gmail.com>
2021-10-03 11:51:46 -0600

    Add to gameData

--- src/Data/GameData.js
+++ src/Data/GameData.js
@@ -1,7 +1,9 @@
 const gameData = {
-  barGames: ["Pool", "Darts", "Pinball"],
-  boardGames: ["Hive", "Ticket to Ride", "Monopoly"],
-  cardGames: ["Nerts", "Speed", "Egyptian Rat Slap"],
+  barGames: ["Pool", "Darts", "Pinball", "Pink Pong"],
+  boardGames: ["Hive", "Ticket to Ride", "Monopoly", "Bananagrams"],
+  cardGames: ["Nerts", "Speed", "Egyptian Rat Slap", "Gin Rummy", "Kings in the Corner"],
+  minuteGames: ["Coin Stack"],
+  wildCard: ["50 States", "50 Universities", "50 Countries", "HORSE", "Frisbee Golf", "Mini Golf"]
 }
 
 export default gameData;
